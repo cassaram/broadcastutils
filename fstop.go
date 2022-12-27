6443a4e77336df8d735ab9ead27f038530c3b959
cassaram <michael@cassara.tech>
2022-12-26 21:07:26 -0500

    Make FStop <-> Int conversions 2 decimal precision

--- fstop.go
+++ fstop.go
@@ -8,19 +8,19 @@ Ex: f2.3
 type FStop float32
 
 /*
-Returns a FStop as an int with 1 decimal precision
+Returns a FStop as an int with 2 decimal precision
 
-Ex: f11.4 -> 114
+Ex: f11.4 -> 1140
 */
 func FStopToInt(fs FStop) int {
-	return int(fs * 10)
+	return int(fs * 100)
 }
 
 /*
-Converts an int to a FStop with 1 decimal precision
+Converts an int to a FStop with 2 decimal precision
 
-Ex: 114 -> f11.4
+Ex: 1140 -> f11.4
 */
 func IntToFStop(fs int) FStop {
-	return FStop(fs / 10)
+	return FStop(fs / 100)
 }
